8f9926086dea172b5450393c95a7e5dccfe50204
Dean Campbell <thedeancampbell@gmail.com>
2020-10-29 22:04:37 -0700

    alert-dialog.component test

--- wowup-electron/src/app/components/alert-dialog/alert-dialog.component.spec.ts
+++ wowup-electron/src/app/components/alert-dialog/alert-dialog.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+import { MockTranslatePipe } from "../../../../tests/app/pipes/translate.pipe.mock";
 import { AlertDialogComponent } from "./alert-dialog.component";
 
 describe("AlertDialogComponent", () => {
@@ -7,7 +13,12 @@ describe("AlertDialogComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AlertDialogComponent],
+      declarations: [AlertDialogComponent, MockTranslatePipe],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
   });
 
